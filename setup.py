e8cb59ae3a17ba28082a83f3f2ccf715d06c220e
Samuel Girardin <samuelgirardin@pm.me>
2021-05-05 09:41:01 +0200

    fix packaging

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     packages=[
         "masonite.js_routes",
         "masonite.js_routes.providers",
-        "masonite.js_routes.controllers",
         "masonite.js_routes.commands",
     ],
     package_dir={"": "src"},
