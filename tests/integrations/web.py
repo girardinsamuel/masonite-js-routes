23cc78f2ba7272e9e4869007a46f0bfa361a3ba3
Samuel Girardin <samuelgirardin@pm.me>
2021-06-28 21:53:57 +0200

    update with latest changes

--- tests/integrations/web.py
+++ tests/integrations/web.py
@@ -1,6 +1,8 @@
 """ Web Routes """
 from masonite.routes import Route
 
-Route.get("/", "WelcomeController@show").name("home"),
-Route.get("posts", "WelcomeController@posts").name("posts.index"),
-Route.get("posts/@id", "WelcomeController@show_post").name("posts.show"),
+ROUTES = [
+    Route.get("/", "WelcomeController@show").name("home"),
+    Route.get("posts", "WelcomeController@posts").name("posts.index"),
+    Route.get("posts/@id", "WelcomeController@show_post").name("posts.show"),
+]
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
-from masonite.foundation import Application, Kernel, HttpKernel
+from masonite.foundation import Application, Kernel
 from tests.integrations.config.providers import PROVIDERS
 import os
+
+from tests.integrations.app.Kernel import Kernel as ApplicationKernel
 
 
 application = Application(os.getcwd())
@@ -8,6 +10,6 @@ application = Application(os.getcwd())
 """First Bind important providers needed to start the server
 """
 
-application.register_providers(Kernel, HttpKernel)
+application.register_providers(Kernel, ApplicationKernel)
 
 application.add_providers(*PROVIDERS)
