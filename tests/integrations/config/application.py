d245e610b901d48955263a90293f0dbd33ecd4ac
Samuel Girardin <samuelgirardin@pm.me>
2022-01-05 12:38:19 +0100

    use app_url from config

--- tests/integrations/config/application.py
+++ tests/integrations/config/application.py
@@ -1,3 +1,12 @@
 from masonite.environment import env
 
-KEY = env("APP_KEY", "APP_KEY=plyUWY8iZnEH9_8WrVjl-LS3B8aRtHK9UAB35fGAq0M=")
+
+KEY = env("APP_KEY", "-RkDOqXojJIlsF_I8wWiUq_KRZ0PtGWTOZ676u5HtLg=")
+
+HASHING = {
+    "default": env("HASHING_FUNCTION", "bcrypt"),
+    "bcrypt": {"rounds": 10},
+    "argon2": {"memory": 1024, "threads": 2, "time": 2},
+}
+
+APP_URL = env("APP_URL", "http://localhost:8000/")
