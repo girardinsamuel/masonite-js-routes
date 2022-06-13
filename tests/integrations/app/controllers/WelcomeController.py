c7e31fae2bdbeaeee223394426235524df995e72
Samuel Girardin <samuelgirardin@pm.me>
2022-06-13 18:23:44 +0200

    fix tests

--- tests/integrations/app/controllers/WelcomeController.py
+++ tests/integrations/app/controllers/WelcomeController.py
@@ -1,4 +1,5 @@
 """A WelcomeController Module."""
+
 from masonite.views import View
 from masonite.controllers import Controller
 
@@ -7,4 +8,16 @@ class WelcomeController(Controller):
     """WelcomeController Controller Class."""
 
     def show(self, view: View):
-        return view.render("welcome")
+        return view.render("base")
+
+    def posts(self, view: View):
+        return "posts"
+
+    def create_post(self):
+        return 201
+
+    def show_post(self, view: View):
+        return "show_post"
+
+    def show_post_comments(self):
+        return 200
